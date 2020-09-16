4856e28d799471b7e1e1dc7df1d95c4fe11392f5
saeeshtends <saeesh@ajency.in>
2020-09-16 13:56:57 +0530

    changes

--- wp-content/themes/healthkart/functions.php
+++ wp-content/themes/healthkart/functions.php
@@ -10,22 +10,6 @@
  * @link    https://www.studiopress.com/
  */
 
-<<<<<<< Updated upstream
-add_action( 'wp_enqueue_scripts', 'my_child_theme_scripts' );
-function my_child_theme_scripts() {
-    wp_enqueue_style( 'parent-theme-css', get_template_directory_uri() . '/style.css' );
-}
-
-//* this will bring in the Genesis Parent files needed:
-include_once( get_template_directory() . '/lib/init.php' );
-
-//* We tell the name of our child theme
-define( 'Child_Theme_Name', __( 'Genesis Child', 'genesischild' ) );
-//* We tell the web address of our child theme (More info & demo)
-define( 'Child_Theme_Url', 'http://gsquaredstudios.com' );
-//* We tell the version of our child theme
-define( 'Child_Theme_Version', '1.0' );
-=======
 // Starts the engine.
 require_once get_template_directory() . '/lib/init.php';
 
@@ -52,15 +36,10 @@ require_once get_stylesheet_directory() . '/lib/customize.php';
 
 // Includes Customizer CSS.
 require_once get_stylesheet_directory() . '/lib/output.php';
->>>>>>> Stashed changes
 
 // Adds WooCommerce support.
 require_once get_stylesheet_directory() . '/lib/woocommerce/woocommerce-setup.php';
 
-<<<<<<< Updated upstream
-//* Add HTML5 responsive recognition
-add_theme_support( 'genesis-responsive-viewport' );
-=======
 // Adds the required WooCommerce styles and Customizer CSS.
 require_once get_stylesheet_directory() . '/lib/woocommerce/woocommerce-output.php';
 
@@ -220,4 +199,3 @@ function genesis_sample_comments_gravatar( $args ) {
 	return $args;
 
 }
->>>>>>> Stashed changes
