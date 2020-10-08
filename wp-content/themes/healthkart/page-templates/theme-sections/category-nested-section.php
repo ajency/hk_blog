3fb67d1a60cfed5c02fbe51600e76628fca96c95
Ankita <ankita@ajency.in>
2020-10-08 11:01:45 +0530

    added category and read time in one line

--- wp-content/themes/healthkart/page-templates/theme-sections/category-nested-section.php
+++ wp-content/themes/healthkart/page-templates/theme-sections/category-nested-section.php
@@ -38,14 +38,14 @@ $categories = get_categories(
 								<div class="recent-post-content p-3">
 									<?php $categories = hk_get_category(get_the_ID()); ?>
 									<span>
-										<div class="category">
+										<span class="category">
 											<?php foreach($categories as $index => $category): ?>
 											<a title="<?php echo $category->name; ?>" href="<?php echo get_category_link($category); ?>" rel="category tag"><?php echo $category->name; ?></a>
 											<?php if($index+1 != count($categories)): ?>
 												,
 											<?php endif; endforeach; ?>
-										</div>
-										<!-- <span class="dot"><i class="fa fa-circle" aria-hidden="true"></i></span> -->
+										</span>
+										<span class="dot"><i class="fa fa-circle" aria-hidden="true"></i></span>
 										<span class="last-read"><?php echo get_mins_read(); ?> MIN READ</span>
 									</span>
 									<div class="recent-post-header">
