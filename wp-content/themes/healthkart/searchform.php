4f88528e48be1e3c89c5dd9750481b44ea12f99d
saeeshtends <saeesh@ajency.in>
2020-10-19 09:39:31 +0530

    input

--- wp-content/themes/healthkart/searchform.php
+++ wp-content/themes/healthkart/searchform.php
@@ -1,6 +1,6 @@
 <form action="/search" method="get" role="search" id="searchform">
 	<div class="input-group">
-		<input type="text" class="form-control search-bar" placeholder="Search Healthkart Blog">
+		<input type="text" name="s" id="search" class="form-control search-bar" placeholder="Search Healthkart Blog" value="<?php the_search_query(); ?>">
 		<div class="input-group-append button-icon">
 			<button class="btn" type="button" id="searchBtn">
 				<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/search-icon.png" alt="search" class="search-icon-white">
