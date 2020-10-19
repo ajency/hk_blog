6589756e06882dbc1def5062318a02de66aee73f
saeeshtends <saeesh@ajency.in>
2020-10-19 09:38:19 +0530

    search button

--- wp-content/themes/healthkart/searchform.php
+++ wp-content/themes/healthkart/searchform.php
@@ -1,9 +1,8 @@
-<form action="/" method="get" role="search" id="searchform">
+<form action="/search" method="get" role="search" id="searchform">
 	<div class="input-group">
 		<input type="text" class="form-control search-bar" placeholder="Search Healthkart Blog">
 		<div class="input-group-append button-icon">
-			<button class="btn" type="button">
-				<input type="submit">
+			<button class="btn" type="button" id="searchBtn">
 				<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/search-icon.png" alt="search" class="search-icon-white">
 				<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/search-icon-grey.png" alt="search" class="search-icon-grey">
 			</button>
