0a804ff9a0f65a1a592b38feb066a619baf477e1
saeeshtends <saeesh@ajency.in>
2020-10-20 17:06:09 +0530

    page not found

--- wp-content/themes/healthkart/404.php
+++ wp-content/themes/healthkart/404.php
@@ -1,3 +1,6 @@
 <?php get_header(); ?>
-<h1>Page not found</h1>
+<div class="container my-4">
+	<h1>Sorry, this page isn't available.</h1>
+	<div class="content">The link you followed may be broken, or the page may have been removed. <a href="/">Go back to home</a></div>
+</div>
 <?php get_footer(); ?>
