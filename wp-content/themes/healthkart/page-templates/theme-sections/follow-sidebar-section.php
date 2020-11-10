a130d49701a8c0b97c72cacfa5ac2a8d9dfd3849
saeeshtends <saeesh@ajency.in>
2020-11-10 12:01:04 +0530

    link

--- wp-content/themes/healthkart/page-templates/theme-sections/follow-sidebar-section.php
+++ wp-content/themes/healthkart/page-templates/theme-sections/follow-sidebar-section.php
@@ -2,7 +2,7 @@
 	<?php $social_data = get_option('wpseo_social'); 
 	foreach ($social_data as $type => $link) : 
 		$type_parts = explode("_", $type); 
-		if($link): ?>
+		if(is_string($link) && $link && count($type_parts) < 3): ?>
 			<a href="<?php echo $link; ?>" class="contact-button-link <?php echo $type_parts[0]; ?>" title="Follow on <?php echo ucwords($type_parts[0]); ?>" target="_blank" style=""><span class="fa fa-<?php echo $type_parts[0]; ?>"></span></a>
 		<?php endif; 
 	endforeach; ?>
