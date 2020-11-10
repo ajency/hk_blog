0eb95650fff64adf6bd9c503b855b0f791066965
saeeshtends <saeesh@ajency.in>
2020-11-10 11:24:53 +0530

    youtube and instagram

--- wp-content/themes/healthkart/page-templates/theme-sections/follow-sidebar-section.php
+++ wp-content/themes/healthkart/page-templates/theme-sections/follow-sidebar-section.php
@@ -2,4 +2,6 @@
 	<a href="https://www.facebook.com/healthkart" class="contact-button-link facebook" title="Follow on Facebook" target="_blank" style=""><span class="fa fa-facebook"></span></a>
 	<a href="https://twitter.com/Healthkart" class="contact-button-link twitter" title="Follow on Twitter" target="_blank" style=""><span class="fa fa-twitter"></span></a>
 	<a href="https://pinterest.com/healthkart" class="contact-button-link pinterest" title="Visit on LinkedIn" target="_blank" style=""><span class="fa fa-pinterest"></span></a>
+	<a href="https://www.instagram.com/healthkart" class="contact-button-link instagram" title="Visit on Instagram" target="_blank" style=""><span class="fa fa-instagram"></span></a>
+	<a href="https://www.youtube.com/healthkart" class="contact-button-link youtube" title="Visit on Youtube" target="_blank" style=""><span class="fa fa-youtube"></span></a>
 </div>
