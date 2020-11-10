43b2dae6adb1da680575f050f855d0140a2e02d7
saeeshtends <saeesh@ajency.in>
2020-11-10 12:10:08 +0530

    link fixes

--- wp-content/themes/healthkart/page-templates/theme-sections/follow-sidebar-section.php
+++ wp-content/themes/healthkart/page-templates/theme-sections/follow-sidebar-section.php
@@ -2,8 +2,8 @@
 	<?php $social_data = get_option('wpseo_social'); 
 	foreach ($social_data as $type => $link) : 
 		$type_parts = explode("_", $type); 
-		if(is_string($link) && $link && count($type_parts) < 3): ?>
-			<a href="<?php echo $link; ?>" class="contact-button-link <?php echo $type_parts[0]; ?>" title="Follow on <?php echo ucwords($type_parts[0]); ?>" target="_blank" style=""><span class="fa fa-<?php echo $type_parts[0]; ?>"></span></a>
+		if(is_string($link) && $link && count($type_parts) == 2): ?>
+			<a href="<?php echo $type_parts[0] == 'twitter' ? 'https://twitter.com/'.$link :$link; ?>" class="contact-button-link <?php echo $type_parts[0]; ?>" title="Follow on <?php echo ucwords($type_parts[0]); ?>" target="_blank" style=""><span class="fa fa-<?php echo $type_parts[0]; ?>"></span></a>
 		<?php endif; 
 	endforeach; ?>
 </div>
