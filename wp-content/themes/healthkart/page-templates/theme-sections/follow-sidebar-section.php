eea83dc5df551f095f420066561190bd9d3221b6
saeeshtends <saeesh@ajency.in>
2020-11-10 11:56:35 +0530

    changes

--- wp-content/themes/healthkart/page-templates/theme-sections/follow-sidebar-section.php
+++ wp-content/themes/healthkart/page-templates/theme-sections/follow-sidebar-section.php
@@ -1,7 +1,9 @@
 <div id="social-follow-sidebar" class="content-desktop">
-	<a href="https://www.facebook.com/healthkart" class="contact-button-link facebook" title="Follow on Facebook" target="_blank" style=""><span class="fa fa-facebook"></span></a>
-	<a href="https://twitter.com/Healthkart" class="contact-button-link twitter" title="Follow on Twitter" target="_blank" style=""><span class="fa fa-twitter"></span></a>
-	<a href="https://pinterest.com/healthkart" class="contact-button-link pinterest" title="Visit on LinkedIn" target="_blank" style=""><span class="fa fa-pinterest"></span></a>
-	<a href="https://www.instagram.com/healthkart" class="contact-button-link instagram" title="Visit on Instagram" target="_blank" style=""><span class="fa fa-instagram"></span></a>
-	<a href="https://www.youtube.com/healthkart" class="contact-button-link youtube" title="Visit on Youtube" target="_blank" style=""><span class="fa fa-youtube"></span></a>
+	<?php $social_data = get_option('wpseo_social'); 
+	foreach ($social_data as $type => $link) : 
+		$type_parts = explode("_", $type); 
+		if($link): ?>
+			<a href="<?php echo $link; ?>" class="contact-button-link <?php echo $type_parts[0]; ?>" title="Follow on <?php echo ucwords($type_parts[0]); ?>" target="_blank" style=""><span class="fa fa-<?php echo $type_parts[0]; ?>"></span></a>
+		<?php endif; 
+	endforeach; ?>
 </div>
