e3f5a214e4447a168e6bf1b5369197748c8c811b
saeeshtends <saeesh@ajency.in>
2020-11-16 14:53:40 +0530

    post

--- wp-content/themes/healthkart/incs/filters.php
+++ wp-content/themes/healthkart/incs/filters.php
@@ -87,5 +87,5 @@ function hk_remove_meta_box( $callback )
 {
     remove_meta_box( 'genesis_inpost_scripts_box' , 'post' , 'normal' );
 }
-add_action( 'add_meta_boxes_page', 'hk_remove_meta_box', 999 );
+add_action( 'add_meta_boxes_post', 'hk_remove_meta_box', 999 );
 
