6ba2a0934bd026a60152042ce70096c61380820b
saeeshtends <saeesh@ajency.in>
2020-11-16 14:57:17 +0530

    amp meta boxes

--- wp-content/themes/healthkart/incs/filters.php
+++ wp-content/themes/healthkart/incs/filters.php
@@ -86,6 +86,8 @@ function hkPrintBreadcrumbs(){
 function hk_remove_meta_box( $callback )
 {
     remove_meta_box( 'genesis_inpost_scripts_box' , 'post' , 'normal' );
+    remove_meta_box( 'pagebilder_content' , 'post' , 'normal' );
+    remove_meta_box( 'ampforwp_title_meta' , 'post' , 'normal' );
 }
 add_action( 'add_meta_boxes_post', 'hk_remove_meta_box', 999 );
 
