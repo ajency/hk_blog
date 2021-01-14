2e078cb7d455c63b5357b8144ee4654ba31daf33
Ankita <ankita@ajency.in>
2021-01-14 15:19:16 +0530

    404 link changed www.healthkart.com/connect/

--- wp-content/themes/healthkart/404.php
+++ wp-content/themes/healthkart/404.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 <div class="container py-5">
 	<h1>Sorry, this page isn't available.</h1>
-	<div class="content">The link you followed may be broken, or the page may have been removed. <a href="/">Go back to home</a></div>
+	<div class="content">The link you followed may be broken, or the page may have been removed. <a href="/connect">Go back to home</a></div>
 </div>
 <?php get_footer(); ?>
