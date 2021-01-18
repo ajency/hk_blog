bac88332d24d130182a159a0d47e8c4069b8ea45
Ankita <ankita@ajency.in>
2021-01-18 15:52:38 +0530

    added GA analytics code

--- wp-content/themes/healthkart/header.php
+++ wp-content/themes/healthkart/header.php
@@ -34,9 +34,18 @@ do_action( 'genesis_title' );
 	<meta charset="utf-8"/>
 	<meta http-equiv="x-ua-compatible" content="ie=edge" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0 height=device-height">
-<?php
-wp_head(); // We need this for plugins.
-?>
+
+	<?php
+	wp_head(); // We need this for plugins.
+	?>
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	  ga('create', 'UA-21820217-1', 'auto');
+	  ga('send', 'pageview');
+	</script>
 </head>
 <?php
 genesis_markup(
