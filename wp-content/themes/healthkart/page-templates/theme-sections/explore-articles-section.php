16283d74a7ae59da3f12f37e1aa403caaeb3548d
Ankita <ankita@ajency.in>
2021-04-21 19:10:08 +0530

    markup cleanup

--- wp-content/themes/healthkart/page-templates/theme-sections/explore-articles-section.php
+++ wp-content/themes/healthkart/page-templates/theme-sections/explore-articles-section.php
@@ -22,7 +22,10 @@
 	<!-- mobile -->
 	<div class="content-mobile">
 		<div class="category-buttons pt-5 explore-cat-slider">
-<button type="button" class="btn m-0 mt-2 mr-3 py-2 px-3 category-buttons-single slick-slide slick-current slick-active" data-val="22" data-slick-index="0" aria-hidden="false" tabindex="-1" role="option" aria-describedby="slick-slide20">Bodybuilding</button>
+			<?php foreach ($categories as $index => $category): ?>
+				<button type="button" class="btn m-0 mt-2 mr-3 py-2 px-3 category-buttons-single <?php if(!$index){
+					echo 'category-buttons-single-active'; $_POST['category_id'] = $category->term_id; }?>" data-val="<?php echo $category->term_id; ?>"><?php echo $category->name; ?></button>
+			<?php endforeach; ?>
 		</div>
 	</div>
 
@@ -33,6 +36,6 @@
 
 	<!-- mobile -->
 	<div class = "category_articles_container position-relative content-mobile">
-		<?php get_template_part( 'page-templates/theme-sections/explore-articles-slider', 'component' ); ?>
+		<?php get_template_part( 'page-templates/theme-sections/explore-articles', 'component' ); ?>
 	</div>
 </div>
