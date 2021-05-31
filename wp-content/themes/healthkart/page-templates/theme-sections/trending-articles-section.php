5c9f330aaf85c67893bb885cbf696ea1c3d50244
chaitanya-ajency <Chaitanya@ajency.in>
2021-05-31 13:24:58 +0530

    test

--- wp-content/themes/healthkart/page-templates/theme-sections/trending-articles-section.php
+++ wp-content/themes/healthkart/page-templates/theme-sections/trending-articles-section.php
@@ -100,7 +100,7 @@
 //     )
 
 		$args = array(
-			'posts_per_page' => 6,
+			'posts_per_page' => 7,
 			'post_type' => array('post'),
 			'post_status' => 'publish',
 			'post__not_in' => $post_ids,
