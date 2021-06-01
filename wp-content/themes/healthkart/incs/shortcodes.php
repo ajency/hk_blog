5ca08f18e444648233b8c89b37e91ed49e5c38f1
Ankita <ankita@ajency.in>
2021-06-01 09:56:51 +0530

    commented code

--- wp-content/themes/healthkart/incs/shortcodes.php
+++ wp-content/themes/healthkart/incs/shortcodes.php
@@ -353,7 +353,7 @@ add_shortcode( 'product-listing', function(){?>
 				$categoryMappingValue = $categoryMappingList[0]; 
 			}
 
-			echo $categoryMappingValue;
+			//echo $categoryMappingValue;
 
 			$api_url = 'https://api.healthkart.com/api/category/all/1';
 
@@ -376,7 +376,7 @@ add_shortcode( 'product-listing', function(){?>
 
 		 			$product_api_url = 'http://api.healthkart.com/api/catalog/results?catPrefix='. $product->catPre.'&pageNo=1&perPage=2&excludeOOS=true&plt=1&st=1';
 
-		 			echo $product->catPre;
+		 			//echo $product->catPre;
 		 		}
 
 	 		}
