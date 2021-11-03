76ba2333e70f8ca59bc10a99a64b1c857e62801c
Mamata <mamata@ajency.in>
2021-11-03 12:21:46 +0530

    added "medically reviewed by" in blog

--- wp-content/themes/healthkart/author.php
+++ wp-content/themes/healthkart/author.php
@@ -36,15 +36,76 @@ get_template_part( 'page-templates/theme-sections/follow-sidebar', 'section' );
 			<div class="row">
 				<div class="col-md-8 col-12 single-post-content">
 					<div class="entry-content row mt-4">
-						<div class="col-4 d-flex justify-content-center">
+					<div class="col-12 d-flex flex-column align-items-center justify-content-center reviewer">
 							<?php 
 							$user_info = get_userdata($post->post_author);
-							echo get_avatar($user_info->ID);
-							?>
-						</div>
-						<div class="col-8">
-							<h2 class="mb-4 p-0"><?php echo $user_info->display_name; ?></h2>
-							<div class="role"><?php echo get_the_author_meta( 'hk_designation', $user_info->ID); ?></div>
+							if (!empty(get_avatar_url( $user_info->ID ))){
+								?><img class="reviewer-avatar" src="<?php echo esc_url( get_avatar_url( $user_info->ID ) ); ?>" /><?php
+							}
+							if (!empty(get_the_author_meta( 'hk_designation', $user_info->ID))){
+								?><p class="reviewer-designation"><?php echo get_the_author_meta( 'hk_designation', $user_info->ID); ?></p>
+							<?php } ?>
+							<h1 class="p-0 reviewer-dname"><?php echo $user_info->display_name; ?></h1>
+								<?php
+									$facebook = get_the_author_meta( 'facebook', $user_info->ID );
+									$instagram = get_the_author_meta( 'instagram', $user_info->ID );
+									$linkedin = get_the_author_meta( 'linkedin', $user_info->ID );
+									$myspace = get_the_author_meta( 'myspace', $user_info->ID );
+									$pinterest = get_the_author_meta( 'pinterest', $user_info->ID );
+									$soundcloud = get_the_author_meta( 'soundcloud', $user_info->ID );
+									$tumblr = get_the_author_meta( 'tumblr', $user_info->ID );
+									$twitter = get_the_author_meta( 'twitter', $user_info->ID );
+									$youtube = get_the_author_meta( 'youtube', $user_info->ID );
+									$wikipedia = get_the_author_meta( 'wikipedia', $user_info->ID );
+									
+							if(!empty($facebook || $instagram || $linkedin || $myspace || $pinterest || $soundcloud || $tumblr || $twitter || $youtube || $wikipedia )){?>
+								<div class="reviewer-social-media">
+											<!-- facebook -->
+											<?php if (!empty($facebook)){ ?>
+											<a href="<?php echo esc_url($facebook); ?>" class="social-media-icons" rel="nofollow" target="_blank"><i class="fa fa-facebook"></i></a>
+											<?php } ?>
+											<!-- instagram -->
+											<?php if (!empty($instagram)){ ?>
+											<a href="<?php echo esc_url($instagram); ?>" class="social-media-icons" rel="nofollow" target="_blank"><i class="fa fa-instagram"></i></a>
+											<?php } ?>
+											<!-- linkedin -->
+											<?php if (!empty($linkedin)){ ?>
+											<a href="<?php echo esc_url($linkedin); ?>" class="social-media-icons" rel="nofollow" target="_blank"><i class="fa fa-linkedin"></i></a>
+											<?php } ?>
+											<!-- myspace -->
+											<?php if (!empty($myspace)){ ?>
+											<a href="<?php echo esc_url($myspace); ?>" class="social-media-icons" rel="nofollow" target="_blank"><i class="fa fa-external-link"></i></a>
+											<?php } ?>
+											<!-- pinterest -->
+											<?php if (!empty($pinterest)){ ?>
+											<a href="<?php echo esc_url($pinterest); ?>" class="social-media-icons" rel="nofollow" target="_blank"><i class="fa fa-pinterest"></i></a>
+											<?php } ?>
+											<!-- soundcloud -->
+											<?php if (!empty($soundcloud)){ ?>
+											<a href="<?php echo esc_url($soundcloud); ?>" class="social-media-icons" rel="nofollow" target="_blank"><i class="fa fa-soundcloud"></i></a>
+											<?php } ?>
+											<!-- tumblr -->
+											<?php if (!empty($tumblr)){ ?>
+											<a href="<?php echo esc_url($tumblr); ?>" class="social-media-icons" rel="nofollow" target="_blank"><i class="fa fa-tumblr"></i></a>
+											<?php } ?>
+											<!-- twitter -->
+											<?php if (!empty($twitter)){ ?>
+											<a href="<?php echo "https://twitter.com/".$twitter; ?>" class="social-media-icons" rel="nofollow" target="_blank"><i class="fa fa-twitter"></i></a>
+											<?php } ?>
+											<!-- youtube -->
+											<?php if (!empty($youtube)){ ?>
+											<a href="<?php echo esc_url($youtube); ?>" rel="nofollow" class="social-media-icons" target="_blank"><i class="fa fa-youtube-play"></i></a>
+											<?php } ?>
+											<!-- wikipedia -->
+											<?php if (!empty($wikipedia)){ ?>
+											<a href="<?php echo esc_url($wikipedia); ?>" rel="nofollow" class="social-media-icons" target="_blank"><i class="fa fa-wikipedia-w"></i></a>
+											<?php } ?>
+								</div>
+							<?php } ?>
+							<?php $reviewer_bio = get_the_author_meta('description');
+							if(!empty($reviewer_bio)){?>
+								<p class="reviewer-bio"> <?php echo $reviewer_bio ?> </p>
+							<?php } ?>
 						</div>
 					</div>  
 					<div class="latest-reads">
