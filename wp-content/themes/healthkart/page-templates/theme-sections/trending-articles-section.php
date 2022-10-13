23e9e0faff0b037c0bbc264277a49c5871586e29
Mahaveer Narvekar <89069801+mrnarvekar@users.noreply.github.com>
2022-10-13 11:40:20 +0530

    Showing trending articles of 2 weeks

--- wp-content/themes/healthkart/page-templates/theme-sections/trending-articles-section.php
+++ wp-content/themes/healthkart/page-templates/theme-sections/trending-articles-section.php
@@ -12,13 +12,21 @@
 					'post__not_in' => $post_ids,
 					'meta_key' => 'hk_views',
 					'order' => 'DESC',
-					'orderby' => 'meta_value_num'
+				'orderby' => 'meta_value_num',
+ 				'date_query' => array(
+     array(
+         'after' => '2 week ago',
+     )
+ )
 				);
+				$count = 0;
 				$main_post = new wp_query( $args );
 				if( $main_post->have_posts() ) :
 					while( $main_post->have_posts() ) :
 						$main_post->the_post(); 
-						$post_ids[] = get_the_id();?>
+						$post_ids[] = get_the_id();
+						$count ++;
+						?>
 						<div class="trending-articles-single trending-articles-single-main col-md-6 col-12">
 							<div class="trending-articles-single-image mb-4"><a href="<?php the_permalink(); ?>" alt="<?php the_title(); ?>" title="<?php the_title(); ?>">
 								<?php if ( has_post_thumbnail() ) {
@@ -31,7 +39,7 @@
 							<div class="trending-articles-single-content">
 								<div class="content-title">
 									<?php $categories = hk_get_category(get_the_ID());  ?>
-									<span>
+									<div class="cat-detail">
 										<span class="category">
 											<?php foreach($categories as $index => $category): ?>
 											<a title="<?php echo $category->name; ?>" href="<?php echo get_category_link($category); ?>" rel="category tag"><?php echo $category->name; ?></a>
@@ -41,14 +49,22 @@
 										</span>
 										<span class="dot"><i class="fa fa-circle" aria-hidden="true"></i></span>
 										<span class="last-read"><?php echo get_mins_read(); ?> MIN READ</span>
-									</span>
+										<span class="dot"><i class="fa fa-circle" aria-hidden="true"></i></span>
+										<?php $post_date = get_the_date( 'M j, Y' ); ?>
+										<span class="last-read"><?php echo $post_date; ?></span>
+									</div>
 									<h2 class="title"><a title="<?php the_title(); ?>" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 								</div>
 								<div class="content-description content-desktop"><?php echo hk_get_excerpt(140); ?></div>
 								<div class="content-description content-mobile"><?php echo hk_get_excerpt(220); ?></div>
 							</div>
 						</div>
-					<?php endwhile;
+					<?php 
+				if($count == 2){
+					break;
+				}
+
+				endwhile;
 				endif; ?>
 			</div>
 			<div class="col-md-4 col-12 sidebar">
@@ -60,6 +76,29 @@
 		<h2 class="trending-articles-heading similar-articles pt-3 pb-3 mt-5 mb-4">EDITOR'S PICK</h2>
 		<div class="row similar-articles">
 		<?php
+
+
+// $date_range = strtotime ( '-15 day' );  
+
+// 		$args = array(
+// 			'posts_per_page' => 6,
+// 			'post_type' => array('post'),
+// 			'post_status' => 'publish',
+// 			'post__not_in' => $post_ids,
+// 			'meta_key' => 'hk_views',
+// 			'order' => 'DESC',
+// 			'orderby' => 'date',
+// 			 // Using the date_query to filter posts from last week
+//     'date_query' => array(
+//        array(
+//                             'after' => array(
+//                                 'year'  => date('Y', $date_range ),
+//                                 'month' => date('m', $date_range ),
+//                                 'day'   => date('d', $date_range ),
+//                             ),
+//                         )
+//     )
+
 		$args = array(
 			'posts_per_page' => 6,
 			'post_type' => array('post'),
@@ -67,13 +106,21 @@
 			'post__not_in' => $post_ids,
 			'meta_key' => 'hk_views',
 			'order' => 'DESC',
-			'orderby' => 'meta_value_num'
+			'orderby' => 'meta_value_num',
+    'date_query' => array(
+        array(
+            'after' => '2 week ago'
+        )
+    )
 		);
+		$count = 0;
 		$main_post = new wp_query( $args );
 		if( $main_post->have_posts() ) :
 			while( $main_post->have_posts() ) :
 				$main_post->the_post(); 
-				$post_ids[] = get_the_id();?>
+				$post_ids[] = get_the_id();
+				$count ++;
+				?>
 				<div class="trending-articles-single row mb-3 col-md-6 col-12 m-0 pl-0 pr-0">
 					<div class="trending-articles-single-image mb-3 col-md-5 col-lg-4 col-12"><a href="<?php the_permalink(); ?>" alt="<?php the_title(); ?>" title="<?php the_title(); ?>">
 						<?php 
@@ -101,15 +148,23 @@
 								</span>
 								<span class="dot"><i class="fa fa-circle" aria-hidden="true"></i></span>
 								<span class="last-read"><?php echo get_mins_read(); ?> MIN READ</span>
+								<span class="dot"><i class="fa fa-circle" aria-hidden="true"></i></span>
+								<?php $post_date = get_the_date( 'M j, Y' ); ?>
+								<span class="last-read"><?php echo $post_date; ?></span>
 							</span>
 							<h2 class="title"><a title="<?php the_title(); ?>" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 						</div>
 					</div>
 				</div>
-			<?php endwhile;
+			<?php 
+			if($count == 6){
+				break;
+			}
+		endwhile;
 		endif; 
 		set_query_var( 'post_ids', $post_ids );
 	?>
 		</div>
 	</div>
-</div>
+	
+	</div>
